8bfc2b6f19417637aa635361343f17017fc5b961
20161104609 <a20161104609@admindeiMac-32.local>
2017-06-13 08:04:40 +0800

    genzheng

--- test  sort/main.c
+++ test  sort/main.c
@@ -10,6 +10,41 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    int a[10];
+    int i,j,temp;
+    printf("请输入10个整数：");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<9;i++)
+    {
+        for(j=i+1;j<10;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+                
+            }
+        }
+    }
+    printf ("请输入顺序：");
+    for(i=0;i<10;i++)
+    {
+        printf("%d",a[i]);
+    }
+    printf("\n");
+    FILE *fp1;
+    fp1=fopen("sort.txt","w+");
+
+        for(i=0;i<11;i++)
+        {
+            scanf("%d",&a[i]);
+            fputc(int a[i],FILE *fp1);
+        }
+        fclose(fp1);
+    
+        return 0;
 }
